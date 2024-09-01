e2781d8ab3bd585f430491e2e2ba26e43cc25dd4
Oleksii Piliai <alekseivpiliai@gmail.com>
2024-09-01 18:30:38 +0200

    starthw1

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const numericContent = parseFloat(content);
-  const numericPadding = parseFloat(padding);
-  const numericBorder = parseFloat(border);
-
-  const totalWidth = numericContent + ((numericPadding + numericBorder) * 2);
-  return totalWidth;
+function checkForSpam(message) {
+    const lowCaseMess = message.toLowerCase();
+    if (lowCaseMess.includes("spam") || lowCaseMess.includes("sale")) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px"));
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
